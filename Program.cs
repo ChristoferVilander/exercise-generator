f7e957ac13e99039e073343227185976893501e4
Christofer <6317669+ChristoferVilander@users.noreply.github.com>
2022-02-05 17:46:39 +0100

    Removed a comment

--- Program.cs
+++ Program.cs
@@ -3,8 +3,7 @@
 {
     public class Program
     {
-        // Application starts here, asking the user to choose A, for an easy list of workouts, or B, 
-        // for an more difficult workout. The character is then passed as a parrameter to the Workout Method. 
+
         public static void Main()
         {
 
